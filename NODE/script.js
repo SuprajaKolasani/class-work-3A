c0fc2fea150dacf8849015668d17669c144cd763
SuprajaKolasani <suprajakolasani@gmail.com>
2024-09-21 10:34:37 +0530

    Add files via upload

---
+++ NODE/script.js
@@ -0,0 +1,23 @@
+console.log("Welcome to Node JS.")
+
+// 1995 - js
+// first js engine name - spiderMonkey - MF
+//                      - V8 ,chrome,edge
+// web dev
+//     - interaction
+//     - FE validation
+// server - express js
+// database - mongoose
+
+// mobile app - react native
+
+// java - jdk(java development )
+//     inside jdk- jre,jvm
+
+
+// NODE - V8 engine(interpret node js) ,c++
+
+
+// const fs = require('fs')
+
+// fs.writeFile('ye')
